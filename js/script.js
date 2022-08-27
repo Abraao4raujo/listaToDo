61aef0a9ddee9b8fdf09557d7fcf32ab308486d7
Abraão Araujo <96948947+Abraao4raujo@users.noreply.github.com>
2022-08-27 05:56:27 -0300

    creating watch in the site

--- js/script.js
+++ js/script.js
@@ -6,6 +6,26 @@ const editForm = document.querySelector("#edit-form")
 const editInput = document.querySelector("#edit-input")
 const cancelEditBtn = document.querySelector("#cancel-edit-btn")
 
+const horas = document.getElementById("horas")
+const minutos = document.getElementById("minutos")
+const segundos = document.getElementById("segundos")
+
+const relogio = setInterval(function time() {
+  let dateToday = new Date();
+  let hora = dateToday.getHours();
+  let min = dateToday.getMinutes();
+  let seg = dateToday.getSeconds();
+
+  if(hora < 10) hora = '0' + hora
+  if(min < 10) min = '0' + min
+  if(seg < 10) seg = '0' + seg
+
+
+  horas.textContent = hora;
+  minutos.textContent = min;
+  segundos.textContent = seg
+})
+
 let oldInputValue;
 
 //funções
